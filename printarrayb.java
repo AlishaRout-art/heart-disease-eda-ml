f8361d19f5b8bf3771c2e6b5a00c5c4db0f51c4a
ALISHA ROUT <routalisha331@gmail.com>
2025-12-30 23:36:54 +0530

    Add files via upload

---
+++ printarrayb.java
@@ -0,0 +1,12 @@
+public class printarrayb {
+    public static void main (String[] args){
+        int[]a=new int[3];
+        a[0]=10;
+        a[1]=20;
+        a[2]=30;
+        for(int b:a){
+System.out.println(b);
+        }
+        
+    }
+}
